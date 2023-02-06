14042066d9e3fb3b6f56f01a38a3d33353eb3437
Doppie22 <dvopstal05@gmail.com>
2023-02-06 20:17:56 +0100

    input field

--- after.js
+++ after.js
@@ -1,4 +1,24 @@
 // this code will be executed after page load
-(function() {
-  console.log('after.js executed');
+(function () {
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  var observer = new MutationObserver(function (mutations, observer) {
+    mutations.forEach((e) => {
+      if (e.target.id === "idBerekening" && e.target.className === "tabsheet active") {
+        const inputbox = e.target.querySelector(".widget").querySelector(".gemairo");
+        if (inputbox === null) {
+          const htmlElement = `<div class='gemairo'>
+                                <input class='gemairo_input' placeholder='Wat wil ik staan?'/>
+                                <input class='gemairo_weging' placeholder='Weging'/>
+                              </div>`;
+          e.target.querySelector(".widget").insertAdjacentHTML("beforeend", htmlElement);
+        }
+      }
+    });
+  });
+
+  observer.observe(document, {
+    subtree: true,
+    attributes: true,
+  });
 })();
